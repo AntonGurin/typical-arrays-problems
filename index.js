23a844b936bb7aa9fd895911e89142a5392e138c
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2021-03-15 17:35:10 +0300

    Create: index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+exports.min = function min (array) {
+  return (Mat.min.apply(Math, array) || 0);
+}
+
+exports.max = function max (array) {
+  return (Math.max.apply(Math, array) || 0);
+}
+
+exports.avg = function avg (array) {
+  if (avg.length == 0){
+    return 0;
+  }   
+}
